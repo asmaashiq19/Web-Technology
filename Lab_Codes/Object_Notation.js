4f9a150a1ce52f336139468decef5d079bc1f8ae
Asma Jan <bhattisha3115@gmail.com>
2026-06-28 18:29:31 +0530

    Create Object_Notation.js

---
+++ Lab_Codes/Object_Notation.js
@@ -0,0 +1,18 @@
+let student={
+  name:"A",
+  age:21,
+  course:"CSE",
+  isStudent:true,
+  address:{
+    locality:"Islamabad",
+    district:"Anantnag",
+    state:"Jammu and Kashmir"
+  },
+  subjects:{
+    "JavaScript",
+    "Python"
+  },
+  console.log(student.subjects[1].name);
+
+      
+    
